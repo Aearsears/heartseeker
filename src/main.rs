aab51c07344925d43ec65f373fd101ceddbd478e
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-03-28 16:57:16 -0400

    strings can't be indexed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    hello(&s1);
+    let x = 5;
+    number(x);
+    number(x);
+}
+fn number(x: i32) {
+    print!("{}", x);
+}
+fn hello(s: &String) {
+    println!("the number is {}", s);
 }
