538ca2bc753ad98e3e0ae5a82c39662ab656ab08
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-03 23:46:25 -0400

    added next steps

--- src/main.rs
+++ src/main.rs
@@ -17,33 +17,38 @@ fn main() {
     }
 }
 fn handle_connection(mut stream: TcpStream) {
-    // read request from client
-    let mut clientbuffer = [0; 1024];
-    let mut proxybuffer = [0; 1024];
-    stream.read(&mut clientbuffer).unwrap();
-    let get = b"GET / HTTP/1.1\r\n";
-    // proxy servers fowards the request to desired URI
-    let req = String::from_utf8_lossy(&clientbuffer).into_owned();
-    println!("Request:{}", req);
-    handle_forward(&req, &mut proxybuffer);
-    println!(
-        "Response from server:{}",
-        String::from_utf8_lossy(&proxybuffer)
-    );
-    stream.write(&proxybuffer).unwrap();
-    stream.flush().unwrap();
+    while true {
+        // read request from client
+        // need to read until newline so that can read entire request for testing with telnet"
+        let mut clientbuffer = [0; 1024];
+        let mut proxybuffer = [0; 1024];
+        stream.read(&mut clientbuffer).unwrap();
+        let get = b"GET / HTTP/1.1\r\n";
+        // proxy servers fowards the request to desired URI
+        let req = String::from_utf8_lossy(&clientbuffer).into_owned();
+        println!("Request:{}", req);
+        handle_forward(&req, &mut proxybuffer);
+        println!(
+            "Response from server:{}",
+            String::from_utf8_lossy(&proxybuffer)
+        );
+        stream.write(&proxybuffer).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 fn handle_forward(req: &String, buffer: &mut [u8]) {
     // find remote ip address of the host by using the ToSocketsAddrs, http is on port 80
     // make sure to have extra line at the very end of the http req otherwise doesn't work
+    // http://pages.cpsc.ucalgary.ca/~carey/CPSC441/ass1/test1.html
     let request =
         "GET /~carey/CPSC441/ass1/test1.html HTTP/1.1\r\nHost: pages.cpsc.ucalgary.ca\r\n\r\n";
     println!("client's request: {}", req);
+    // TODO: parse host name from request
     // create TCP stream connection to host TCPStream::connect
     let mut stream = TcpStream::connect("pages.cpsc.ucalgary.ca:80").unwrap();
     // send the request to the host stream.write(), stream.flush() out request
-    stream.write(request.as_bytes()).unwrap();
+    stream.write(req.as_bytes()).unwrap();
     stream.flush().unwrap();
     //wait for the response and read it stream.read()
     // https://www.jmarshall.com/easy/http/
