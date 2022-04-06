103466c3d0fbce8fb7a97b46dbcc5a9ce49fd17a
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-06 15:22:28 -0400

    refactored gethostname from string into function

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,10 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     loop {
         // read request from client
-        // need to read until newline so that can read entire request for testing with telnet"
         let mut clientbuffer = String::with_capacity(1024);
         let mut proxybuffer = [0; 1024];
         let mut reader = BufReader::with_capacity(1024, &stream);
-        // cannot read one line, need to read line until hit only only CRLF character and then break loop
+        // cannot read one line, need to read line until hit only two CRLF character and then break loop
         let crlf = String::from("\r\n\r\n");
         let mut checkcrlf = String::with_capacity(1024);
         while !checkcrlf.ends_with(&crlf) {
@@ -52,46 +51,36 @@ fn handle_forward(req: &String, buffer: &mut [u8]) {
     // find remote ip address of the host by using the ToSocketsAddrs, http is on port 80
     // make sure to have extra line at the very end of the http req otherwise doesn't work
     // http://pages.cpsc.ucalgary.ca/~carey/CPSC441/ass1/test1.html
-    // let request =
     // "GET /~carey/CPSC441/ass1/test1.html HTTP/1.1\r\nHost: pages.cpsc.ucalgary.ca\r\n\r\n";
-    let split: Vec<&str> = req.split("\r\n").collect();
+
+    // create TCP stream connection to host
+    let hostname: &str = get_hostname(req);
+    println!("hostname:{}", hostname);
+
+    let mut stream = TcpStream::connect(format!("{}{}", &hostname, &String::from(":80"))).unwrap();
+    // send the request to the host stream.write(), stream.flush() out request
+    stream.write(req.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    //wait for the response and read it stream.read()
+    // https://www.jmarshall.com/easy/http/
+
+    stream.read(buffer).unwrap();
+    //return response and close connection
+    stream.shutdown(Shutdown::Both).unwrap();
+}
+
+fn get_hostname<'a>(request: &'a String) -> &'a str {
+    let split: Vec<&str> = request.split("\r\n").collect();
     let mut hostsplit: Vec<&str> = Vec::new();
     for elem in &split {
         if elem.starts_with("Host: ") {
             hostsplit = elem.split("Host: ").collect();
         }
     }
-    // create TCP stream connection to host
-    let hostname = hostsplit.get(1);
-    println!("hostname:{}", hostname.unwrap());
-    match hostname {
-        Some(str) => {
-            let mut stream =
-                TcpStream::connect(format!("{}{}", *str, &String::from(":80"))).unwrap();
-            // send the request to the host stream.write(), stream.flush() out request
-            stream.write(req.as_bytes()).unwrap();
-            stream.flush().unwrap();
-            //wait for the response and read it stream.read()
-            // https://www.jmarshall.com/easy/http/
-
-            stream.read(buffer).unwrap();
-            //return response and close connection
-            stream.shutdown(Shutdown::Both).unwrap();
-        }
+    match hostsplit.get(1) {
+        Some(&str) => &str,
         None => {
             panic!("No hostname in request!")
         }
     }
 }
-
-// fn get_hostname<'a>(request: &'a str) -> &&'a str {
-//     let request = "GET /~carey/CPSC441/ass1/test1.html HTTP/1.1\r\nHost: pages.cpsc.ucalgary.ca\r\nConnection: keep-alive\r\n\r\n";
-//     let split: Vec<&str> = request.split("\r\n").collect();
-//     // let hostsplit: Vec<&str>;
-//     for elem in &split {
-//         if elem.starts_with("Host: ") {
-//             let hostsplit: Vec<&str> = elem.split("Host: ").collect();
-//             hostsplit.get(1).unwrap()
-//         }
-//     }
-// }
