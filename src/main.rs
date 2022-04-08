e6451a97743c71e92e7af1a5169cd0d8dc79e1db
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-07 23:52:03 -0400

    started on gui

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+mod gui;
 mod proxy;
 mod threadpool;
 mod webserver;
 
 fn main() {
-    let pool = threadpool::ThreadPool::new(2);
+    let pool = threadpool::ThreadPool::new(3);
     println!("Backend started...");
     pool.execute(|| {
         proxy::start_proxy(String::from("127.0.0.1:4000"));
     });
     pool.execute(|| webserver::start_admin_page(String::from("127.0.0.1:4001")));
+    pool.execute(|| gui::start_ui().unwrap());
 }
