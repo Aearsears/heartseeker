fe4b29cc20918a707098189e3a9347283aa06ca8
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-15 22:52:56 -0400

    refactored logging fn

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use chrono::{TimeZone, Utc};
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::io::Write;
 use std::path::Path;
 use std::time::{SystemTime, UNIX_EPOCH};
 mod gui;
@@ -20,32 +20,39 @@ fn main() {
     });
     pool.execute(|| webserver::start_admin_page(String::from("127.0.0.1:4001")));
 }
-// FIX permissions error
+
 fn logging(msg: String) {
     let path = Path::new("logging.txt");
     let pathdisplay = path.display();
     let now = SystemTime::now();
     let since_the_epoch = now.duration_since(UNIX_EPOCH).expect("Time went backwards");
-    let mut file = match File::open(&path) {
+    let date = Utc.timestamp(since_the_epoch.as_secs().try_into().unwrap(), 0);
+
+    let mut file = match std::fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(path)
+    {
         Ok(file) => file,
-        Err(why) => {
-            println!("Could not open {}, creating a new copy...", pathdisplay);
-            let mut newfile = match File::create(&path) {
-                Ok(file) => file,
-                Err(why) => panic!(
-                    "Could not create {} because of error {} aborting...",
+        Err(why) => match why.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File not found.");
+            }
+            std::io::ErrorKind::PermissionDenied => {
+                panic!("You don't have the sufficient permissions to open the file.");
+            }
+            _ => {
+                panic!(
+                    "Something went wrong while trying to open {} : {}",
                     pathdisplay, why
-                ),
-            };
-            match newfile.write_all(format!("[{:?}]:{}", since_the_epoch, msg).as_bytes()) {
-                Ok(_) => println!("Logged to {}", pathdisplay),
-                Err(why) => println!("Could not write to {}. Error: {}", pathdisplay, why),
-            };
-            return;
-        }
+                );
+            }
+        },
     };
-    match file.write_all(format!("[{:?}]:{}", since_the_epoch, msg).as_bytes()) {
-        Ok(_) => println!("Logged to {}", pathdisplay),
-        Err(why) => println!("Could not write to {}. Error: {}", pathdisplay, why),
+    match file.write_all(format!("[{}]:{}\r\n", date.to_rfc2822(), msg).as_bytes()) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("Couldn't write to file: {}", e);
+        }
     };
 }
