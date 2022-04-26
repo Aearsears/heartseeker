03acb231e3230b0a814d6b21a379e31b570c83ce
Aearsears <53102185+Aearsears@users.noreply.github.com>
2022-04-25 22:17:02 -0400

    parse message will capture reason code correctly

--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -18,7 +18,7 @@ pub fn parse_message(message: &String, sort: Transactions) -> HashMap<String, St
                 hashmap.insert(d.0.to_string(), d.1.trim().to_string());
             }
             None => {
-                let reqline = line.split(" ");
+                let reqline = line.splitn(3, " ");
                 for (i, part) in reqline.enumerate() {
                     match sort {
                         Transactions::Req => {
@@ -61,6 +61,14 @@ mod tests {
         let req = "HTTP/1.0 200 OK\r\nDate: Fri, 31 Dec 1999 23:59:59 GMT\r\nContent-Type: text/html\r\nContent-Length: 1354\r\n\r\n".to_string();
         let hashmap = parse_message(&req, Transactions::Res);
         assert_eq!(&"OK".to_string(), hashmap.get("Reason").unwrap());
+    }
+
+    #[test]
+    fn test_parse_request_response_reason_2() {
+        let req = "HTTP/1.0 404 Not Found\r\nDate: Fri, 31 Dec 1999 23:59:59 GMT\r\nContent-Type: text/html\r\nContent-Length: 1354\r\n\r\n".to_string();
+        let hashmap = parse_message(&req, Transactions::Res);
+        println!("{}", hashmap.get("Reason").unwrap());
+        assert_eq!(&"Not Found".to_string(), hashmap.get("Reason").unwrap());
     }
 
     #[test]
